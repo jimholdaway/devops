c6609b4aca8ba311da649aa01c7529269100aec9
Jim Holdaway <jim.holdaway@protonmail.com>
2021-01-10 17:42:31 +0100

    initial commit

---
+++ ubuntu-20_04/initial-setup.sh
@@ -0,0 +1,34 @@
+#/bin/bash
+
+# Update system
+echo Updating the system...
+apt update && apt upgrade -y
+
+# Disable password, root and IPV6 connections over SSH
+echo Disabling password, root and IPV6 connections over SSH...
+sed -i -e "s/PermitRootLogin yes/PermitRootLogin no/" /etc/ssh/sshd_config
+sed -i -e "s/#PermitRootLogin no/PermitRootLogin no/" /etc/ssh/sshd_config
+sed -i -e "s/PasswordAuthentication yes/PasswordAuthentication no/" /etc/ssh/sshd_config
+sed -i -e "s/#PasswordAuthentication no/PasswordAuthentication no/" /etc/ssh/sshd_config
+sed -i -e "s/AddressFamily any/AddressFamily inet/" /etc/ssh/sshd_config
+sed -i -e "s/#AddressFamily any/AddressFamily inet/" /etc/ssh/sshd_config
+sed -i -e "s/#AddressFamily inet/AddressFamily inet/" /etc/ssh/sshd_config
+sed -i -e "s/AddressFamily inet6/AddressFamily inet/" /etc/ssh/sshd_config
+sed -i -e "s/#AddressFamily inet6/AddressFamily inet/" /etc/ssh/sshd_config
+
+# Simple SSH only UFW firewall
+echo Writing simple SSH only UFW (firewall) rules...
+sed -i -e "s/IPV6=yes/IPV6=no/" /etc/default/ufw
+ufw default deny incoming
+ufw default allow outgoing
+ufw allow OpenSSH
+ufw disable
+ufw --force enable
+
+# Set kernel parameters to deactivate IPV6
+# Note: Normal to edit sysctl and make chnage persistent to achieve this
+#       At present a bug in Ubuntu 20.04/linode/both? prevents this change
+#       From being persistent. Thus kernel parameters are set in grub config
+echo Updating kernel parameters to disable IPV6...
+sed '/GRUB_CMDLINE_LINUX_DEFAULT/ s/"$/ ipv6.disable=1"/' /etc/default/grub
+sed '/GRUB_CMDLINE_LINUX/ s/"$/ ipv6.disable=1"/' /etc/default/grub
